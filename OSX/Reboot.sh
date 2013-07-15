36d7e5f09c12472eb63c030d2a0db5bd02b8f3ed
Phraust <yourstruly@phraust.com>
2013-07-15 10:44:37 -1000

    Create Reboot.sh

---
+++ OSX/Reboot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Reboot.sh
+
+#################
+# NOTES & USAGE #
+#################
+
+# This script simply texts me whenver the system reboots.
+
+
+/path/to/SMS.py 1234567890 'SERVER REBOOTED.'
+
+
+#####################################
+#    Brought to you by Phraust Co.  #
+#   "Mining For A Better Tomorrow." #
+# 1nbTFZTFy8Wtbti3LhnLFZgd5YTBrXjaU #
+#####################################
